059325f5d071d523ff9a2fcba3f2ab5ca7c37c12
persibcicadas26 <persibcicadas26@gmail.com>
2025-12-23 22:33:53 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,49 @@
+// const Hapi = require('@hapi/hapi');
+// const routes = require('./routes');
+
+// const init = async () => {
+//   const server = Hapi.server({
+//     port: 5000,
+//     host: 'localhost',
+//     routes: {
+//       cors: { origin: ['*'] },
+//     },
+//   });
+
+//   server.route(routes);
+
+//   await server.start();
+//   console.log(`Server berjalan pada ${server.info.uri}`);
+// };
+
+// init();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const port = process.env.PORT ? Number(process.env.PORT) : 5000;
+    const host = process.env.HOST || '0.0.0.0';
+
+    const server = Hapi.server({
+        port,
+        host,
+        routes: {
+            cors: {
+                origin: ['*'],
+            }
+        }
+    });
+
+    server.route(require('./routes'));
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
+
